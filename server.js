ef1f2d53d6c9b317932e548e5914e31bc9f4f01d
YiyueMaggieMao <myykitty711@gmail.com>
2019-10-26 14:14:39 -0700

    successfully set up local host

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+const server = http.createServer(app);
+
+const bodyParser = require('body-parser');
+
+// Server will always find an open port.
+const port = process.env.PORT || 3001;
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening on port ${port}`);
+});
+
+// Default endpoint 
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/fern/index.html");
+});
+
+// Needed to process body parameters for POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+

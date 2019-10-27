db7d83411e9a605545a44c3c6d98f498e8fbf5bb
YiyueMaggieMao <myykitty711@gmail.com>
2019-10-27 08:30:35 -0700

    Added signup page that allows data pushing

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
-
 const bodyParser = require('body-parser');
+
 // Configurations for firebase
 var firebaseConfig = {
     apiKey: "AIzaSyCqCe93WLs0HBzjpnSASjglXCPRtS_VVj8",
@@ -29,14 +29,11 @@ require("firebase/firestore");
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Server will always find an open port.
-const port = process.env.PORT || 3001;
-server.listen(port, '0.0.0.0', () => {
-    console.log(`Server listening on port ${port}`);
-});
-
+// Needed to process body parameters for POST requests
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 // Directories used
-app.use(express.static("fern/public"));
+app.use(express.static(__dirname+"/fern/public"));
 
 // Default endpoint 
 app.get('/', (req, res) => {
@@ -44,6 +41,18 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/fern/public/index.html");
 });
 
-// Needed to process body parameters for POST requests
-app.use(bodyParser.urlencoded({ extended: true }));
+// Methods for posting onto the database
+app.post('/users', (req, res) => {
+    // you have address available in req.body:
+    console.log(req.body);
+    // always send a response:
+    res.json({ ok: true });
+});
 
+
+
+// Server will always find an open port.
+const port = process.env.PORT || 3001;
+server.listen(port, '0.0.0.0', () => {
+    console.log(`Server listening on port ${port}`);
+});

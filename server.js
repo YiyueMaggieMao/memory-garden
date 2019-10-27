6fe0b41d0c86bcaed60b1f5af496d6ffd07cef8f
YiyueMaggieMao <myykitty711@gmail.com>
2019-10-26 23:30:45 -0700

    Incorporated Firebase remote

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+import * as firebase from "firebase/app";
+
+// Add the Firebase services that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+
 const express = require('express');
 const http = require('http');
 const app = express();
 const server = http.createServer(app);
 
 const bodyParser = require('body-parser');
+
+// Configurations for firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyCqCe93WLs0HBzjpnSASjglXCPRtS_VVj8",
+    authDomain: "memory-garden.firebaseapp.com",
+    databaseURL: "https://memory-garden.firebaseio.com",
+    projectId: "memory-garden",
+    storageBucket: "memory-garden.appspot.com",
+    messagingSenderId: "869210082385",
+    appId: "1:869210082385:web:9da5e605a85b29d7e73c70",
+    measurementId: "G-TJBEDJL3DF"
+};
+
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 // Server will always find an open port.
 const port = process.env.PORT || 3001;
@@ -16,7 +46,7 @@ app.use(express.static("fern/public"));
 
 // Default endpoint 
 app.get('/', (req, res) => {
-   // res.sendFile(__dirname + "/fern/public/index.html");
+    // res.sendFile(__dirname + "/fern/public/index.html");
     res.sendFile(__dirname + "/fern/public/index.html");
 });
 

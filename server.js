cde9545bf171f28fd24d5682b80682b95bfdbea0
YiyueMaggieMao <myykitty711@gmail.com>
2019-10-26 18:15:18 -0700

    Made fern/public folder public

--- server.js
+++ server.js
@@ -11,8 +11,12 @@ server.listen(port, '0.0.0.0', () => {
     console.log(`Server listening on port ${port}`);
 });
 
+// Directories used
+app.use(express.static("fern/public"));
+
 // Default endpoint 
 app.get('/', (req, res) => {
+   // res.sendFile(__dirname + "/fern/public/index.html");
     res.sendFile(__dirname + "/fern/public/index.html");
 });
 
